cee20e4a1e618d75b2094ec8e558b2c0605adbd5
Mohamed Elbahja <mohamed.jss11@gmail.com>
2019-01-27 22:49:34 +0100

    rewrite class

--- src/Systempay.php
+++ src/Systempay.php
@@ -12,163 +12,130 @@ namespace Restoore\Systempay;
 use Illuminate\Support\Facades\Config;
 
 class Systempay
-
 {
 
-    private $_key;
-    private $_url;
-    private $_params = array();
-    private $_nb_products = 0;
+	protected $siteId;
+	protected $key;
+	protected $url;
+	protected $params;
+
+	/**
+	 * Systempay constructor
+	 * 
+	 * @param string $config Config name
+	 */
+	public function __construct(string $config = 'systempay')
+	{
+		$config = Config::get($config);
+		$this->siteId  = $config['id'];
+		$this->key     = $config['key'];
+		$this->url     = $config['url'];
+		$this->params  = $config['params'] ?: [];
+
+		// Set mode
+		$this->set('ctx_mode', $config['mode'])->set('site_id', $this->siteId);
+	}
 
 
-    /**
-     * Systempay constructor
-     * @param       String $site_id unique shop id
-     */
-    public function __construct($site_id)
-    {
-        $this->_key = env("SYSTEMPAY_{$site_id}_KEY", Config::get("systempay.{$site_id}.key"));
-        $this->_url = Config::get("systempay.url");
-        //Instanciate parameters array if exist in config file
-        $this->_params = Config::get("systempay.{$site_id}.params");
-        //Directly instanciate the transaction parameters
-        $this->set_site_id($site_id);
-        $this->set_ctx_mode(env("SYSTEMPAY_{$site_id}_ENV", Config::get("systempay.{$site_id}.env")));
-    }
+	/**
+	 * Set a form param
+	 * @param string $param  Param without "vads_"
+	 * @param string $value
+	 */
+	public function set(string $param, string $value): self
+	{
+		$this->params["vads_{$param}"] = $value;
 
-    /**
-     * Magic method that allows you to use getters and setters on each systempay parameters
-     * Remember to not keep the 'vads' prefix in your accessor function name
-     * @param       String $method name of the accessor
-     * @param       array [optional] $args list of arguments
-     * @return      \App\Library\Systempay
-     * @throws      InvalidArgumentException
-     */
-    public function __call($method, $args)
-    {
-        if (function_exists($method)) {
-            return call_user_func_array($method, $args);
-        }
-        if (preg_match("/get_(.*)/", $method, $matches)) {
-            return $this->_params["vads_{$matches[1]}"];
-        }
-        if (preg_match("/set_(.*)/", $method, $matches)) {
-            if (count($args) != 1)
-                throw new InvalidArgumentException($method . ' takes one argument.');
+		return $this;
+	}
 
-            $this->_params["vads_{$matches[1]}"] = $args[0];
-            return $this;
-        }
-    }
+	/**
+	 * Get param value
+	 * @param  string $param
+	 * @return string|null
+	 */
+	public function get(string $param)
+	{
+		return $this->params[$param] ?? null;
+	}
 
-    /**
-     * Method to do massive assignement of parameters
-     * @param           array $params associative array of systempay parameters
-     * @return          \App\Library\Systempay
-     */
-    public function set_params($params)
-    {
-        $this->_params = array_merge($this->_params, $params);
-        return $this;
-    }
 
-    /**
-     * Get all systempay parameters
-     * @return      array associative array of systempay parameters
-     */
-    public function get_params()
-    {
-        return $this->_params;
-    }
+	/**
+	 * Check is param exists
+	 * @param  string  $param
+	 * @return boolean
+	 */
+	public function has(string $param): bool
+	{
+		return isset($this->params["vads_{$param}"]);
+	}
 
-    /**
-     * Generate systempay signature and add it to the parameters array
-     * @return      \App\Library\Systempay
-     */
-    public function set_signature()
-    {
-        ksort($this->_params);
-        $s = "";
-        foreach ($this->_params as $n => $v)
-            $s .= $v . "+";
-        $s .= $this->_key;
-        $this->_params['signature'] = sha1($s);
+	/**
+	 * Remove a param
+	 * @param  string $param
+	 * @return self
+	 */
+	public function remove(string $param): self
+	{
+		if ($this->has($param)) {
 
-        return $this;
-    }
+			unset($this->params[$param]);
+		}
+	}
 
-    /**
-     * Return systempay signature
-     * @return      String systempay signature
-     */
-    public function get_signature()
-    {
-        return $this->_params['signature'];
-    }
+	/**
+	 * Enabling/disabling 3D Secure
+	 * @param  bool $enable
+	 * @return self
+	 */
+	public function set3ds(bool $enable = true): self
+	{
+		return $this->set('threeds_mpi', ($enable ? 0 : 2));
+	}
 
-    /**
-     * Defines the total amount of the order. If you doesn't give the amount in parameter, it will be automaticly calculated
-     * by the sum of products you've got in your basket
-     * @param       [optional] int $amount, systempay format
-     * @return      \App\Library\Systempay
-     */
-    public function set_amount($amount = 0)
-    {
-        $this->_params['vads_amount'] = 0;
-        if ($amount) {
-            $this->_params['vads_amount'] = 100 * $amount;
-        } else {//calcul du montant à partir du tableau de paramètre
-            array_where($this->_params, function ($value, $key) {
-                if (preg_match("/vads_product_amount([0-9]+)/", $key, $match)) {
-                    $this->_params['vads_amount'] += $this->_params["vads_product_qty{$match[1]}"] * $value;
-                }
-            });
-        }
-        return $this;
-    }
+	/**  
+	 * Get all params
+	 * @return array
+	 */
+	public function getParams(): array
+	{
+		return $this->params;
+	}
 
-    /**
-     * Get total amount of the order
-     * @param       [optional] bool $decimal if true, you get a decimal otherwise you get standard systempay amount format (int)
-     * @return      float
-     */
-    public function get_amount($decimal = true)
-    {
-        return $decimal ? $this->_params['vads_amount'] / 100 : $this->_params['vads_amount'];
-    }
 
-    /**
-     * Return HTML SystempPay form
-     * @param       String $button html code of the submit button
-     * @return      \Illuminate\Support\HtmlString|string
-     */
-    public function get_form($button)
-    {
-        $html_form = '<form method="post" action="' . $this->_url . '" accept-charset="UTF-8">';
-        foreach ($this->_params as $key => $value)
-            $html_form .= '<input type="hidden" name="' . $key . '" value="' . $value . '">';
-        $html_form .= '<input type="hidden" name="var_trans_date" value="'.gmdate('YmdHis').'">';
-        $html_form .= $button;
-        $html_form .= "</form>";
-        return $html_form;
-    }
+	/**
+	 * Return systempay signature
+	 * @return      String systempay signature
+	 */
+	public function getSignature()
+	{
+		$data = '';
+		ksort($this->params);
 
-    /**
-     * Add a product to the order
-     * @param       array $product , must have the following keys : 'label,amount,type,ref,qty'
-     * @return      \App\Library\Systempay
-     */
-    public function add_product($product)
-    {
-        $this->_params = array_merge($this->_params, [
-            "vads_product_label{$this->_nb_products}" => $product["label"],
-            "vads_product_amount{$this->_nb_products}" => $product["amount"] * 100,
-            "vads_product_type{$this->_nb_products}" => $product["type"],
-            "vads_product_ref{$this->_nb_products}" => $product["ref"],
-            "vads_product_qty{$this->_nb_products}" => $product["qty"]
-        ]);
-        $this->_params['vads_nb_products'] = $this->_nb_products += 1;
-        return $this;
-    }
+		foreach ($this->params as $v)
+		{
+			$data .= "{$v}+";
+		}
 
+		return sha1($data . $this->key);
+	}
+
+	/**
+	 * Get the form
+	 * @param  string $btn Costum html button
+	 * @return string
+	 */
+	public function getForm(string $btn): string
+	{
+		$this->set('trans_date', gmdate('YmdHis'));
+
+		$form = '<form method="POST" action="'. $this->url .'" accept-charset="UTF-8">';
+
+		foreach ($this->params as $k => $v)
+		{
+			$form .= '<input type="hidden" name="'. $k .'" value="'. $v .'">';
+		}
+
+		return $form . '<input type="hidden" name="signature" value="'. $this->getSignature() .'" >' . $btn . '</form>';
+	}
 }
--- src/config/systempay.php
+++ src/config/systempay.php
@@ -1,21 +1,35 @@
 <?php
 
-return [
+return 
+[
+    /**
+     * Your website id
+     */
+    'id' => 'YOURE_SITE_ID',
 
-    'YOUR_SITE_ID' => [
-        'key' => 'YOUR_KEY',
-        'env' => 'PRODUCTION',
-        'params' => [
-            //Put here your generals payment call parameters
-            'vads_page_action' => 'PAYMENT',
-            'vads_action_mode' => 'INTERACTIVE',
-            'vads_payment_config' => 'SINGLE',
-            'vads_page_action' => 'PAYMENT',
-            'vads_version' => 'V2',
-            'vads_currency' => '978'
-        ]
+    /**
+     * Your key
+     */
+    'key' => env('SYSTEMPAY_KEY', 'YOUR_KEY'),
+    
+    /**
+     * PRODUCTION or TEST
+     */
+    'mode' => env('SYSTEMPAY_MODE', 'PRODUCTION'),
+
+    /**
+     * Default params
+     */
+    'params' => 
+    [
+        'vads_page_action' => 'PAYMENT',
+        'vads_action_mode' => 'INTERACTIVE',
+        'vads_payment_config' => 'SINGLE',
+        'vads_page_action' => 'PAYMENT',
+        'vads_version' => 'V2',
+        'vads_currency' => '978'
     ],
+
     //Systempay's url
     'url' => 'https://paiement.systempay.fr/vads-payment/',
-
 ];
